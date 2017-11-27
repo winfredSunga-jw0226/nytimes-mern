cdfc83e725b451162b18faf88bfdf14a64e02690
Winfred Sunga <winfredsunga01@gmail.com>
2017-11-26 19:43:20 -0800

    bug fix

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(routes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, function() {
