6e883c88fa848b9f9461ff3a4552f243fadfc411
Winfred Sunga <winfredsunga01@gmail.com>
2017-11-26 18:40:03 -0800

    editing the package.json file

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const apiRoutes = require("./api");
 
 //use API routes
 router.use("/api", apiRoutes);
-console.log("I am inside the index.js of routes folder!");
 
 //if no API routes are hit, send the React app
 router.use(function(req, res) {
